70b4723c8ae05a16e7a0b033618aac94837fa7f7
fahmyfauzi <fahmyfauzii@gmail.com>
2022-02-26 14:50:11 +0700

    fresh

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Faker\Factory;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $faker = Factory::create('id_id');
+
+        // foreach (range(1, 10) as $item) {
+        //     User::create([
+        //         'name' => $faker->name,
+        //         'email' => $faker->email,
+        //         'password' => Hash::make('password'),
+        //     ]);
+        // }
+    }
+}
